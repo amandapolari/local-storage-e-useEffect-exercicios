f6b5963f901abe309f94657dc6de8c1fac310e60
Amanda <amandaoliveirapolari@gmail.com>
2023-06-20 15:07:57 -0300

    Correção de identação de configurações

--- src/App.js
+++ src/App.js
@@ -1,88 +1,83 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { DivContainer, InputsContainer, ListaDeTarefas, Tarefa } from './style';
 
-
-
 function App() {
-  const [tarefas, setTarefas] = useState([]);
-  const [valorDoInput, setValorDoInput] = useState("");
-  const [filtro, setFiltro] = useState("")
+    const [tarefas, setTarefas] = useState([]);
+    const [valorDoInput, setValorDoInput] = useState('');
+    const [filtro, setFiltro] = useState('');
 
-  // useEffect() => {
-  //   () => {
+    // useEffect() => {
+    //   () => {
 
-  //   },
-  //   []
-  // };
+    //   },
+    //   []
+    // };
 
-  // useEffect() => {
-  //   () => {
+    // useEffect() => {
+    //   () => {
 
-  //   },
-  //   []
-  // };
+    //   },
+    //   []
+    // };
 
-  const pegarValorDoInput = (event) => {
-    console.log("aaa");
-  }
+    const pegarValorDoInput = (event) => {
+        console.log('aaa');
+    };
 
-  const criarTarefa = () => {
-    console.log("aaa");
-  }
+    const criarTarefa = () => {
+        console.log('aaa');
+    };
 
-  const selecionarTarefa = (id) => {
-    console.log("aaa");
-  }
+    const selecionarTarefa = (id) => {
+        console.log('aaa');
+    };
 
-  const pegarValorDoSelect = (event) => {
-    console.log("aaa");
-  }
+    const pegarValorDoSelect = (event) => {
+        console.log('aaa');
+    };
 
+    const listaFiltrada = tarefas.filter((tarefa) => {
+        switch (filtro) {
+            case 'pendentes':
+                return !tarefa.completa;
+            case 'completas':
+                return tarefa.completa;
+            default:
+                return true;
+        }
+    });
 
-  const listaFiltrada = tarefas.filter(tarefa => {
-    switch (filtro) {
-      case 'pendentes':
-        return !tarefa.completa
-      case 'completas':
-        return tarefa.completa
-      default:
-        return true
-    }
-  });
+    return (
+        <DivContainer>
+            <h1>Lista de tarefas</h1>
+            <InputsContainer>
+                <input value={valorDoInput} onChange={pegarValorDoInput} />
+                <button onClick={criarTarefa}>Adicionar</button>
+            </InputsContainer>
+            <br />
 
-
-  return (
-    <DivContainer>
-      <h1>Lista de tarefas</h1>
-      <InputsContainer>
-        <input value={valorDoInput} onChange={pegarValorDoInput} />
-        <button onClick={criarTarefa}>Adicionar</button>
-      </InputsContainer>
-      <br />
-
-      <InputsContainer>
-        <label>Filtro</label>
-        <select value={filtro} onChange={pegarValorDoSelect}>
-          <option value="">Nenhum</option>
-          <option value="pendentes">Pendentes</option>
-          <option value="completas">Completas</option>
-        </select>
-      </InputsContainer>
-      <ListaDeTarefas>
-        {listaFiltrada.map(tarefa => {
-          return (
-            <Tarefa
-              completa={tarefa.completa}
-              onClick={() => selecionarTarefa(tarefa.id)}
-            >
-              {tarefa.texto}
-            </Tarefa>
-          )
-        })}
-      </ListaDeTarefas>
-    </DivContainer>
-  )
+            <InputsContainer>
+                <label>Filtro</label>
+                <select value={filtro} onChange={pegarValorDoSelect}>
+                    <option value="">Nenhum</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="completas">Completas</option>
+                </select>
+            </InputsContainer>
+            <ListaDeTarefas>
+                {listaFiltrada.map((tarefa) => {
+                    return (
+                        <Tarefa
+                            completa={tarefa.completa}
+                            onClick={() => selecionarTarefa(tarefa.id)}
+                        >
+                            {tarefa.texto}
+                        </Tarefa>
+                    );
+                })}
+            </ListaDeTarefas>
+        </DivContainer>
+    );
 }
 
-
-export default App
+export default App;
